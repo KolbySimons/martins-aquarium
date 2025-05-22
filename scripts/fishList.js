0602869c4ee7871059d5f8590c637bfaff6238d7
Kolby Simons <kolby.simons@gmail.com>
2025-05-22 13:32:23 -0500

    Current css

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -10,7 +10,8 @@ export const fishList = () => {
              <img src="${fish.image}" alt="${fish.species} image" class="fish_image">
               <div class="fish_details">
                   <h2 class="fish_name">${fish.name}</h2>
-                  <p class="fish_species">${fish.species}</p>
+                  <p class="fish_species">Is a ${fish.species} that is ${fish.length} inches long</p>
+                  
                   
               </div>
           </article>
@@ -28,6 +29,7 @@ export const fishList = () => {
     console.error('Could not find element with id "fishList"');
   }
 }; */
-/*<p class="fish_length>${fish.length} inches</p>
-  <p class="fish_location>${fish.location}</p>
-  <p class="fish_diet>${fish.diet}</p> */
+/*<p class="fish_location>${fish.location}</p>
+                  <p class="fish_diet>${fish.diet}</p>
+                  <p class="fish_length>${fish.length} inches long</p>
+ */
